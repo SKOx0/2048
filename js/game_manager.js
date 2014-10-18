94f4175ef38e30af9de285cf2a377d5bf179d714
Victor Mesquita <contato@nerddownloads.com>
2014-10-18 16:38:39 -0300

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,14 +1,4 @@
-var updateScoreInterval,
-    setType = 'continue',
-    oldObject = {},
-    curObject = {};
-
-var currentBtcBalance = (parseFloat($('#userCurrentBalance').html())).toFixed(8);
-
 function GameManager(size, InputManager, Actuator, StorageManager) {
-  // if(getScore().grid){
-  //   this.grid = JSON.parse(getScore().grid);
-  // }
   this.size           = size; // Size of the grid
   this.inputManager   = new InputManager;
   this.storageManager = new StorageManager;
@@ -23,83 +13,31 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.setup();
 }
 
-// get user current Score
-function getScore(){
-  var _token = document.getElementById('_token').value,
-      userId = document.getElementById('current_user').value;
-
-  var userScores = JSON.parse($.ajax({
-    type: 'POST',
-    url : '/getScore',
-    async: false,
-    data: {
-      userId : userId,
-      _token : _token
-    },
-    dataType: 'json'
-  }).responseText);
-  return userScores.records;
-}
-
-// update score
-// function setScore(bestScore, score, grid, milestone, type){
-//   var _token = document.getElementById('_token').value,
-//       userId = document.getElementById('current_user').value,
-//       milestone = milestone;
-//   var timeoutTime = type == 'reset' ? 0 : 1000;
-
-//   // console.log(grid);
-
-//   // score list
-//   // var bestScore = window.bestScore,
-//   //     score     = window.score;
-
-//   updateScoreInterval = setTimeout(function(){
-//     $.ajax({
-//       type: 'POST',
-//       url : '/updateScore',
-//       data: {
-//         type : type,
-//         userId : userId,
-//         score: score,
-//         grid : JSON.stringify(grid),
-//         milestone: JSON.stringify(milestone),
-//         curBalance: currentBtcBalance,
-//         bestScore: bestScore,
-//         _token : _token
-//       },
-//       dataType: 'json'
-//     }).done(function(response){
-//       $('#userCurrentBalance').html(response.account_balance);
-//     });
-//   }, timeoutTime);  
-// }
-
-
 // Restart the game
 GameManager.prototype.restart = function () {
   this.storageManager.clearGameState();
-  this.actuator.continueGame(); // Clear the game won/lost message
+  this.actuator.continue(); // Clear the game won/lost message
   this.setup();
 };
 
 // Keep playing after winning (allows going over 2048)
 GameManager.prototype.keepPlaying = function () {
   this.keepPlaying = true;
-  this.actuator.continueGame(); // Clear the game won/lost message
+  this.actuator.continue(); // Clear the game won/lost message
 };
 
 // Return true if the game is lost, or has won and the user hasn't kept playing
 GameManager.prototype.isGameTerminated = function () {
-  return this.over || (this.won && !this.keepPlaying);
+  if (this.over || (this.won && !this.keepPlaying)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Set up the game
 GameManager.prototype.setup = function () {
   var previousState = this.storageManager.getGameState();
-  // console.log(JSON.parse(getScore().grid));
-  // console.log(previousState);
-  // previousState =  JSON.parse(getScore().grid);
 
   // Reload the game from a previous game if present
   if (previousState) {
@@ -126,11 +64,9 @@ GameManager.prototype.setup = function () {
 
 // Set up the initial tiles to start the game with
 GameManager.prototype.addStartTiles = function () {
-  this.storageManager.clearStateLog("");
   for (var i = 0; i < this.startTiles; i++) {
     this.addRandomTile();
   }
-  this.storageManager.clearStateLog(this.storageManager.getStateLog().replace("#",":"));
 };
 
 // Adds a tile in a random position
@@ -138,20 +74,16 @@ GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     var value = Math.random() < 0.9 ? 2 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
-    this.storageManager.addRandomTilesStateLog(value);
+
     this.grid.insertTile(tile);
   }
 };
 
 // Sends the updated grid to the actuator
 GameManager.prototype.actuate = function () {
-  this.storageManager.addCurrentTilesStateLog(this.grid.getCurrentTileStr());
   if (this.storageManager.getBestScore() < this.score) {
     this.storageManager.setBestScore(this.score);
-    window.bestScore = this.score;
   }
-
-  window.score = this.score;
 
   // Clear the state when the game is over (game over only, not win)
   if (this.over) {
@@ -160,33 +92,13 @@ GameManager.prototype.actuate = function () {
     this.storageManager.setGameState(this.serialize());
   }
 
-  // console.log(this.serialize());
-
   this.actuator.actuate(this.grid, {
-      score:      this.score,
-      over:       this.over,
-      won:        this.won,
-      bestScore:  this.storageManager.getBestScore(),
-      terminated: this.isGameTerminated()
-    },
-    this
-  ); 
-  
-  // Creates a timer that will cause the AI to make a single move.
-  GameManager.prototype.startAI = function () {
-  var self = this;
-  if (this.isGameTerminated()) return; // Don't do anything if the game's over
-  if(this.timeout_ai != null) {
-  clearTimeout(this.timeout_ai)
-  this.timeout_ai = null;
-  }
-  var manager = this;
-  this.timeout_ai = setTimeout(function(){
-  self.timeout_ai = null;
-  self.move(JS_MinimaxBestMove(self.grid.cells));
-  self.startAI();
-  }, 250);
-  } 
+    score:      this.score,
+    over:       this.over,
+    won:        this.won,
+    bestScore:  this.storageManager.getBestScore(),
+    terminated: this.isGameTerminated()
+  });
 
 };
 
@@ -220,7 +132,6 @@ GameManager.prototype.moveTile = function (tile, cell) {
 
 // Move tiles on the grid in the specified direction
 GameManager.prototype.move = function (direction) {
-  //console.log(direction);
   // 0: up, 1: right, 2: down, 3: left
   var self = this;
 
@@ -235,14 +146,13 @@ GameManager.prototype.move = function (direction) {
   // Save the current tile positions and remove merger information
   this.prepareTiles();
 
-  // Traverse the grid in the right direction and move tiles  
+  // Traverse the grid in the right direction and move tiles
   traversals.x.forEach(function (x) {
     traversals.y.forEach(function (y) {
       cell = { x: x, y: y };
-      tile = self.grid.cellContent(cell);  
+      tile = self.grid.cellContent(cell);
 
       if (tile) {
-            //console.log(tile);
         var positions = self.findFarthestPosition(cell, vector);
         var next      = self.grid.cellContent(positions.next);
 
@@ -282,6 +192,200 @@ GameManager.prototype.move = function (direction) {
 
     this.actuate();
   }
+/******************************************/  
+/******************************************/
+/*    THE AI CODE STARTS FROM HERE        */
+/******************************************/
+/******************************************/
+  /*  0:Up 
+      1:Right 
+      2:Down  
+      3:Left 
+  */
+
+this.isValid = function(x,y){
+  if(x < 0 || x >3 || y <0 || y > 3)
+    return false;
+  return true;
+}  
+this.moveCells = function(matrix, move){
+  var dx = [-1,0,1,0];
+  var dy = [0,1,0,-1];
+  var nx,ny;
+  for(var k = 0;k<3;k++){
+    for(var i = 0;i<4;i++){
+      for(var j = 0; j<4; j++){
+       nx = i+dx[move];
+       ny = j+dy[move];
+       if(self.isValid(nx,ny)){
+          if(matrix[nx][ny] == 0){
+           matrix[nx][ny] = matrix[i][j];
+           matrix[i][j] = 0;
+         }
+        }
+      }
+    }
+  }
+  for(var i = 0;i<4;i++){
+      for(var j = 0; j<4; j++){
+        nx = i + dx[move];
+        ny = j + dy[move];
+        if(self.isValid(nx,ny)){
+          if(matrix[i][j] == matrix[nx][ny]){
+            matrix[nx][ny] *= -2;
+            matrix[i][j] = 0;
+          }
+        }
+      }
+    }
+    for(var k = 0;k<3;k++){
+    for(var i = 0;i<4;i++){
+      for(var j = 0; j<4; j++){
+        if(matrix[i][j] <0)
+          matrix[i][j] *= -1;
+        nx = i+dx[move];
+        ny = j+dy[move];
+        if(self.isValid(nx,ny)){
+          if(matrix[nx][ny] == 0){
+           matrix[nx][ny] = matrix[i][j];
+           matrix[i][j] = 0;
+          }
+        }
+      }
+    }
+  }
+  return matrix;
+}
+
+this.evaluateMatrix = function(matrix){
+  /* Count Number of Free Spaces */
+  var cc = 0;
+  for(var i = 0;i<4;i++)
+    for(var j = 0;j<4;j++){
+      if(matrix[i][j] == 0)
+        cc += 100;
+      else 
+        cc += matrix[i][j]*matrix[i][j];
+    }
+
+  return cc;
+}
+
+this.printMatrix = function(matrix){
+  for(var i = 0;i<4;i++){
+    var str = ""
+    for(var j = 0;j<4;j++)
+      str += matrix[i][j] + " ";
+    console.log(str)
+  }
+  console.log("******************************");
+}
+
+this.findFreeCell = function(matrix){
+  var i,j,k=0;
+  do{
+    i =  (Math.floor(Math.random()*100))%4;
+    j =  (Math.floor(Math.random()*100))%4;
+    k++;
+  }while(matrix[i][j] != 0 && k != 500);
+  if(matrix[i][j] != 0)
+    for(i = 0;i<4;i++)
+      for(j = 0;j<4;j++)
+        if(matrix[i][j] == 0)
+          return ({x:i, y:j});
+  
+  return ({x:i, y:j});
+}
+
+this.isEqualMatrix = function(m1,m2){
+  for(var i = 0;i<4;i++)
+    for(var j = 0;j<4;j++)
+      if(m1[i][j] != m2[i][j])
+        return false;
+  return true;
+}
+
+this.minMax = function(matrix, move, depth){
+  if(depth == 6)
+    return 0;
+  var rmatrix = self.moveCells(self.createCopy(matrix),move);
+  var areSame = self.isEqualMatrix(rmatrix, matrix);
+  var score = self.evaluateMatrix(rmatrix);
+
+  if(areSame == true)
+    return score-1;
+  var maxVal=-1000,val,ret;
+  var freeCell = self.findFreeCell(rmatrix);
+  if(freeCell.x == 4 || freeCell.y == 4)
+    console.log("YES VALUE IS 4 || " + freeCell.x + " | " + freeCell.y);
+  rmatrix[freeCell.x][freeCell.y] = 2;
+  for(var x = 0;x<4;x++)
+  {
+    val = this.minMax(self.createCopy(rmatrix), x, depth+1);
+    if(val > maxVal)
+      maxVal  = val;
+  }
+  return (score+maxVal);
+}
+
+  this.getMove = function(matrix){
+
+    var maxVal = 0,val,ret;
+    for(var x = 0; x < 4;x++){
+      val = this.minMax(self.createCopy(matrix),x,0);
+      // console.log("Score for "+ x + ":" + val )
+      if(val > maxVal){
+        maxVal = val;
+        ret = x;
+      }
+    }
+    return ret;
+  }
+  
+  
+  this.getMatrix = function(){
+    var matrix = [];
+    for (var i = 0 ; i <4 ; i++) {
+      var row = [];
+      for (var j = 0; j < 4; j++) {
+        tile = self.grid.cellContent({x:j, y:i});
+        if(tile == null)
+          row.push(0);
+        else 
+          row.push(tile["value"]);
+      };
+      matrix.push(row);
+    };
+    return matrix;
+  }
+
+  this.createCopy = function(matrix){
+    var ret =[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    for(var i = 0; i < 4;i++)
+      for(var j = 0; j < 4; j++)
+        ret[i][j] = matrix[i][j].valueOf();
+    return ret;
+  }
+
+  
+
+ 
+setTimeout(function() {
+  matrix = self.getMatrix();
+  var myMove = self.getMove(self.createCopy(matrix));
+  var rmat = self.moveCells(self.createCopy(matrix), myMove);
+  console.log(myMove);
+  if( self.isEqualMatrix(rmat,matrix))
+    myMove = (Math.floor(Math.random()*100))%4;
+  self.move(myMove);
+  }, 100);
+
+/******************************************/  
+/******************************************/
+/*        THE AI CODE ENDS  HERE          */
+/******************************************/
+/******************************************/
+
 };
 
 // Get the vector representing the chosen direction
